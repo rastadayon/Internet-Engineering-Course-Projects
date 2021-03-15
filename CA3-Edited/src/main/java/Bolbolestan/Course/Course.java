a9cf3e0d4abe8117f893ab50a79c758be848c4fe
Ghazal <kalhorghazal1378@gmail.com>
2021-03-15 10:36:19 +0000

    Change attribute name

--- CA3-Edited/src/main/java/Bolbolestan/Course/Course.java
+++ CA3-Edited/src/main/java/Bolbolestan/Course/Course.java
@@ -3,19 +3,19 @@ package Bolbolestan.Course;
 import java.util.ArrayList;
 
 public class Course {
-    private String courseCode;
+    private String code;
     private String name;
     private int units;
     private ArrayList<String> prerequisites = new ArrayList<>();
 
-    public Course(String courseCode, String name, int units, ArrayList<String> prerequisites) {
-        this.courseCode = courseCode;
+    public Course(String code, String name, int units, ArrayList<String> prerequisites) {
+        this.code = code;
         this.name = name;
         this.units = units;
         this.prerequisites = prerequisites;
     }
 
-    public String getCourseCode() { return courseCode; }
+    public String getCourseCode() { return code; }
 
     public String getName() { return name; }
 
