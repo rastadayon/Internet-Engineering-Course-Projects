c7a89c5ea177b0acf0c60f32420fe0b44ca39942
Rasta <rasta.tadayon1378@gmail.com>
2021-04-21 15:54:46 +0430

    Successfully add student info (except for student image)

--- CA5/front-end/react_app/src/pages/home/Home.js
+++ CA5/front-end/react_app/src/pages/home/Home.js
@@ -22,14 +22,16 @@ export default class Home extends React.Component {
         API.get("student/").then(
             jsonData => {
                 this.setState({studentInfo: jsonData.data});
+                console.log('in updateStudent info')
                 console.log(this.state.studentInfo.id)
         }).catch(error => {
+            if(error.response.status != 200)
+                window.location.href = "http://localhost:3000/login"
             console.log('rid')
-            window.location.href = "http://localhost:3000/login"
         })
     }
 
-    async componentDidMount() {
+    componentDidMount() {
         document.title = "Home";
         this.updateStudentInfo()
     }
@@ -42,11 +44,36 @@ export default class Home extends React.Component {
                 <div className="container-fluid text-center">
                     <div className="main row">
                     <ProfileInfo studentInfo = {this.state.studentInfo}/>
+                    {/* {this.ProfileInfo()} */}
                     </div>
                 </div>
                 <Footer/>
             </div>
         );
     }
+
+    // ProfileInfo() {
+    //     // console.log(studentInfo)
+    //     // console.log(typeof props.studentInfo)
+    //     if(this.state.studentInfo != undefined)
+    //         console.log(this.state.studentInfo.id)
+    //     return (
+    //         <div className="std-info col-sm-3">
+    //             {/* <img className="avatar" src="../assets/images/robot.png" alt="avatar"/> */}
+    //             <ul>
+    //                 <li>نام: <span className="std-name">{this.state.studentInfo.id}</span></li>
+    //                 <li>شماره دانشجویی: <span className="std-id">۸۱۰۱۹۶۰۰۰</span></li>
+    //                 <li>تاریخ تولد: <span className="std-birthday">۱۳۷۸/۱/۱</span></li>
+    //                 <li>معدل کل: <span className="std-gpa">۱۷.۸۲</span></li>
+    //                 <li>واحد گذرانده: <span className="std-units">۹۴.۰۰</span></li>
+    //                 <li>دانشکده: <span className="std-campus">پردیس دانشکده‌های فنی</span></li>
+    //                 <li>رشته: <span className="std-major">مهندسی کامپیوتر</span></li>
+    //                 <li>مقطع: <span className="std-level>">کارشناسی</span></li>
+    //                 <li><span className="std-status">مشغول به تحصیل</span></li>
+    //             </ul>
+
+    //         </div>
+    //     );
+    // }
 
 }
--- CA5/front-end/react_app/src/pages/home/ProfileInfo/ProfileInfo.js
+++ CA5/front-end/react_app/src/pages/home/ProfileInfo/ProfileInfo.js
@@ -4,39 +4,27 @@ import PropTypes, { shape } from 'prop-types'
 import "./profileinfo-styles.css"
 
 function ProfileInfo(props) {
-    console.log('in profile info')
-    console.log(props.studentInfo.map())
+    console.log(props.studentInfo)
+    // console.log(typeof props.studentInfo)
+    // console.log(props.studentInfo.entries())
+
     return (
         <div className="std-info col-sm-3">
-            {/* <img className="avatar" src="../assets/images/robot.png" alt="avatar"/> */}
+            {/* <img className="avatar" src={props.studentInfo ? props.studentInfo.img : '-'} alt="avatar"/> */}
             <ul>
-                <li>نام: <span className="std-name">{props.name}</span></li>
-                <li>شماره دانشجویی: <span className="std-id">۸۱۰۱۹۶۰۰۰</span></li>
-                <li>تاریخ تولد: <span className="std-birthday">۱۳۷۸/۱/۱</span></li>
+                <li>نام: <span className="std-name">{props.studentInfo ? props.studentInfo.name + ' ' + props.studentInfo.secondName : '-'}</span></li>
+                <li>شماره دانشجویی: <span className="std-id">{props.studentInfo ? props.studentInfo.id : '-'}</span></li>
+                <li>تاریخ تولد: <span className="std-birthday">{props.studentInfo ? props.studentInfo.birthDate : '-'}</span></li>
                 <li>معدل کل: <span className="std-gpa">۱۷.۸۲</span></li>
                 <li>واحد گذرانده: <span className="std-units">۹۴.۰۰</span></li>
-                <li>دانشکده: <span className="std-campus">پردیس دانشکده‌های فنی</span></li>
-                <li>رشته: <span className="std-major">مهندسی کامپیوتر</span></li>
-                <li>مقطع: <span className="std-level>">کارشناسی</span></li>
-                <li><span className="std-status">مشغول به تحصیل</span></li>
+                <li>دانشکده: <span className="std-campus">{props.studentInfo ? props.studentInfo.faculty : '-'}</span></li>
+                <li>رشته: <span className="std-major">{props.studentInfo ? props.studentInfo.field : '-'}</span></li>
+                <li>مقطع: <span className="std-level>">{props.studentInfo ? props.studentInfo.level : '-'}</span></li>
+                <li><span className="std-status">{props.studentInfo ? props.studentInfo.status : '-'}</span></li>
             </ul>
 
         </div>
     );
-}
-
-ProfileInfo.propTypes = {
-    studentInfo:  PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        secondName: PropTypes.string.isRequired,
-        birthDate: PropTypes.string.isRequired,
-        field: PropTypes.string.isRequired,
-        status: PropTypes.string.isRequired,
-        faculty: PropTypes.string.isRequired,
-        level: PropTypes.string.isRequired,
-        img: PropTypes.string.isRequired
-    })
 }
 
 export default ProfileInfo;
